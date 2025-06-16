31241887f8a64abf864148a4282a4f0b5654f0fb
Christopher Parnell <u7292105@anu.edu.au>
2025-06-17 06:23:36 +1000

    Refactored

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -1,35 +1,51 @@
 using ArgCheck
 
 """
-    bisection(f, inita::Real, initb::Real; tol::Real = 1e-16, maxiter::Int = 1000)
+    function bisection(
+        f, a::Real, b::Real;
+        xtol::Real = 1e-16, ftol::Real = 1e-16, maxiter::Int = 1000
+    )
 
-Compute a root of the function `f` between `inita` and `initb` with tolerance
-`tol` and maximum iterations `maxiter`. Might return `inita` or `initb` if 
-tolerably close to a root.
+Compute a root of the function `f` on the interval `[a, b]`.
 
-Default values `tol = 1e-16` and `maxiter = 1000` are used if unspecified.
+# Arguments
+- `f`: Real to Real function
+- `a::Real`: lower interval bound
+- `b::Real`: upper interval bound
+- `xtol::Real = 1e-16`: domain tolerance
+- `ftol::Real = 1e-16`: codomain tolerance
+- `maxiter::Int = 1000`: maximum iterations
+
+# Returns
+- `(root::Real, f(root)::Real, xerror::Real, noiter::Int, convergence::Bool)`
 """
-function bisection(f, inita::Real, initb::Real; tol::Real = 1e-16, maxiter::Int = 1000)
-    @argcheck inita < initb "lower bound must be smaller than upper bound"
-    @argcheck tol > 0 "tolerance must be positive"
-    a, b, c = inita, initb, nothing
+function bisection(
+    f, a::Real, b::Real;
+    xtol::Real = 1e-16, ftol::Real = 1e-16, maxiter::Int = 1000
+)
+    @argcheck a < b "lower bound must be smaller than upper bound"
+    @argcheck xtol > 0 && ftol > 0 "tolerances must be positive"
+    u, v, e, n = f(a)::Real, f(b)::Real, b-a, 0
+    @argcheck u * v <= 0 "sign must change on the interval"
 
-    fa, fb, fc = f(a)::Real, f(b)::Real, nothing
-    @argcheck fa * fb <= 0 "sign must change on the interval"
-    if abs(fa) < tol return a end
-    if abs(fb) < tol return b end
+    while true
+        e = e/2
+        m = a + e
+        w = f(m)::Real
+        n += 1
 
-    for _ in 1:maxiter
-        c = (a + b)/2
-        fc = f(c)::Real
+        if e < xtol
+            return (m, w, e, n, true)
+        elseif abs(w) < ftol
+            return (m, w, e, n, true)
+        elseif n == maxiter
+            return (m, w, e, n, false)
+        end
 
-        if abs(fc) < tol
-            return c
-        elseif fa * fc < 0
-            b, fb = c, fc
+        if u * w <= 0
+            b, v = m, w
         else
-            a, fa = c, fc
+            a, u = m, w
         end
     end
-    error("bisection did not converge in $maxiter iterations, reached f($c) = $fc")
 end
