6b16759951b4f948b9efb484bebc5bb444a3e2e8
Christopher Parnell <u7292105@anu.edu.au>
2025-06-29 18:41:51 +1000

    _cardinal_function() implementation in preparation of creating interpolating polynomials in Lagrange form

--- src/PolynomialInterpolation.jl
+++ src/PolynomialInterpolation.jl
@@ -20,7 +20,7 @@ struct NewtonPolynomial <: InterpolationPolynomial
 end
 
 function (ip::InterpolationPolynomial)(x::Real)
-    return sum(ip.coeffs[i] * p.base_polys[i](x) for i in eachindex(p.coeffs))
+    return sum(ip.coeffs[i] * ip.base_polys[i](x) for i in eachindex(p.coeffs))
 end
 
 function inter_poly_str(ip::InterpolationPolynomial)
@@ -29,6 +29,53 @@ end
 
 function create_lagrange()
     return
+end
+
+function _cardinal_functions(xs::Vector{<:Real})
+    len = length(xs)
+    funcs = BasisPolynomial[]
+
+    for n = 1:len
+        # (x-xs[1])...(x-xs[n-1])(x-xs[n+1])...(x-xs[len]) evaluated at xs[n]
+        denom = prod((xs[n] - xs[i]) for i in 1:len if i != n)
+        # (x-xs[1])...(x-xs[n-1])(x-xs[n+1])...(x-xs[len])/(evaluated denom)
+        func = (x -> prod((x - xs[i]) for i in 1:len if i != n) / denom)
+
+        # build string of form [(x-xs[.])...(x-xs[.])]/(evaluated denom)
+        segments = String[]
+        if (sign(denom) == -1)
+            push!(segments, "-")
+            denom = -denom
+        end
+        push!(segments, "[")
+        for i in 1:len
+            if (i == n)
+                continue
+            elseif (sign(xs[i]) == 1)
+                xsi = xs[i]
+                segment = "(x - $xsi)"
+                push!(segments, segment)
+            elseif (sign(xs[i]) == -1)
+                xsi = -xs[i]
+                segment = "(x + $xsi)"
+                push!(segments, segment)
+            else
+                segment = "x"
+                push!(segments, segment)
+            end
+        end
+        push!(segments, "]")
+        if (denom > 1)
+            push!(segments, "/$denom")
+        end
+        string = join(segments)
+
+        # instantiate a BasisPolynomial with corresponding function and string
+        basis_poly = BasisPolynomial(func, string)
+        push!(funcs, basis_poly)
+    end
+
+    return funcs
 end
 
 function create_newton()
@@ -42,4 +89,3 @@ end
 function newton_to_standard()
     return
 end
-
